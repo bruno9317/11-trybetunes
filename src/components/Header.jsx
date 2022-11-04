5be05cc435ab23dfc9013caf77e124270a63b3e8
bruno <brunobaptista9317@gmail.com>
2022-11-04 12:59:56 -0300

    salvando requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -23,7 +24,6 @@ export default class Header extends Component {
     const { name, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        {/* <h1>header</h1> */}
         <div
           data-testid="header-user-name"
         >
@@ -34,6 +34,9 @@ export default class Header extends Component {
               </h1>
             )}
         </div>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Search</Link>
       </header>
     );
   }
