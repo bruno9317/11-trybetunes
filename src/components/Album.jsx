f03ed1dbd54a982b63783aee00094930db3418c0
bruno <brunobaptista9317@gmail.com>
2022-11-04 17:03:05 -0300

    salvando requisito 6

---
+++ src/components/Album.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default class Album extends Component {
+  render() {
+    const { albumImage, albumName, artistName, collectionId } = this.props;
+    return (
+      <div>
+        <img src={ albumImage } alt={ albumName } />
+        <h2>{ albumName }</h2>
+        <h4>{ artistName }</h4>
+        <Link
+          to={ `/album/${collectionId}` }
+          data-testid={ `link-to-album-${collectionId}` }
+        >
+          Search
+        </Link>
+      </div>
+    );
+  }
+}
+
+Album.propTypes = {
+  albumImage: PropTypes.string.isRequired,
+  albumName: PropTypes.string.isRequired,
+  artistName: PropTypes.string.isRequired,
+  collectionId: PropTypes.number.isRequired,
+};
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Album from '../components/Album';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class search extends Component {
   state = {
     pesquisar: '',
     isButtonDisabled: true,
+    isLoading: false,
+    fraseResultado: '',
+    albums: [],
   };
 
   handleChange = (event) => {
@@ -17,29 +23,73 @@ export default class search extends Component {
     });
   };
 
+  handleSearchClick = async () => {
+    const { pesquisar } = this.state;
+    this.setState({ isLoading: true });
+    const receba = await searchAlbumsAPI(pesquisar);
+    this.setState({
+      isLoading: false,
+      fraseResultado: `Resultado de álbuns de: ${pesquisar}`,
+      albums: receba,
+      pesquisar: '',
+    });
+  };
+
   render() {
     const {
       pesquisar,
       isButtonDisabled,
+      albums,
+      isLoading,
+      fraseResultado,
     } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input
-          type="text"
-          placeholder="pesquisar"
-          name="pesquisar"
-          value={ pesquisar }
-          onChange={ this.handleChange }
-          data-testid="search-artist-input"
-        />
-        <button
-          type="button"
-          disabled={ isButtonDisabled }
-          data-testid="search-artist-button"
-        >
-          Pesquisar
-        </button>
+        {isLoading ? <Loading />
+          : (
+            <div>
+              <input
+                type="text"
+                placeholder="pesquisar"
+                name="pesquisar"
+                value={ pesquisar }
+                onChange={ this.handleChange }
+                data-testid="search-artist-input"
+              />
+              <button
+                type="button"
+                disabled={ isButtonDisabled }
+                onClick={ this.handleSearchClick }
+                data-testid="search-artist-button"
+              >
+                Pesquisar
+              </button>
+            </div>)}
+        <h3>
+          {
+            `${fraseResultado}`
+          }
+        </h3>
+        {
+          albums.length
+          > 0
+            ? (
+              <h1>
+                { albums.map((p, index) => (<Album
+                  key={ index }
+                  albumImage={ p.artworkUrl100 }
+                  albumName={ p.collectionName }
+                  artistName={ p.artistName }
+                  collectionId={ p.collectionId }
+                />)) }
+              </h1>
+            ) : (
+              <h1>
+                Nenhum álbum foi encontrado
+              </h1>
+            )
+        }
       </div>
     );
   }
