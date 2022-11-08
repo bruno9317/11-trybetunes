ae462d97f48bd194f99217a285cee0f87e3dfb25
bruno <brunobaptista9317@gmail.com>
2022-11-08 11:07:28 -0300

    salvando requisito 12

--- src/components/FavoriteMusicCard.jsx
+++ src/components/FavoriteMusicCard.jsx
@@ -13,37 +13,31 @@ export default class FavoriteMusicCard extends Component {
       albumImage,
     } = this.props;
     return (
-      <div className="card mb-3">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src={ albumImage }
-              alt={ musicName }
-              className="rounded-start"
-              width="100%"
-              height="100%"
-            />
-          </div>
-          <div className="col-md-8">
-            <div className="cardOthers">
-              <h3>{musicName}</h3>
-              <audio data-testid="audio-component" src={ previewUrl } controls>
-                <track kind="captions" />
-              </audio>
-              <label htmlFor={ trackId }>
-                <input
-                  type="checkbox"
-                  id={ trackId }
-                  name="favorites"
-                  data-testid={ `checkbox-music-${trackId}` }
-                  onClick={ onInputClick }
-                  defaultChecked
-                />
-                Favorita
-              </label>
-            </div>
-          </div>
+      <div className="musicCard rounded-start rounded-end">
+        <img
+          src={ albumImage }
+          alt={ musicName }
+          className="rounded-start"
+          // width="50px"
+          height="100%"
+        />
+        <div>
+          <h3>{musicName}</h3>
+          <audio data-testid="audio-component" src={ previewUrl } controls>
+            <track kind="captions" />
+          </audio>
         </div>
+        <label htmlFor={ trackId }>
+          <input
+            type="checkbox"
+            id={ trackId }
+            name="favorites"
+            data-testid={ `checkbox-music-${trackId}` }
+            onClick={ onInputClick }
+            defaultChecked
+          />
+          Favorita
+        </label>
       </div>
     );
   }
--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -13,35 +13,30 @@ export default class MusicCard extends Component {
       albumImage,
     } = this.props;
     return (
-      <div className="card mb-3">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              src={ albumImage }
-              alt={ musicName }
-              className="rounded-start"
-              width="100%"
-              height="100%"
-            />
-          </div>
-          <div className="col-md-8">
-            <div className="cardOthers">
-              <h3 className="card-title">{musicName}</h3>
-              <audio data-testid="audio-component" src={ previewUrl } controls>
-                <track kind="captions" />
-              </audio>
-              <label htmlFor={ trackId }>
-                <input
-                  type="checkbox"
-                  id={ trackId }
-                  name="Favorites"
-                  data-testid={ `checkbox-music-${trackId}` }
-                  onClick={ onInputClick }
-                  defaultChecked={ isChecked }
-                />
-                Favorita
-              </label>
-            </div>
+      <div className="musicCard rounded-start rounded-end">
+        <img
+          src={ albumImage }
+          alt={ musicName }
+          className="rounded-start"
+          height="100%"
+        />
+        <div className="corpoCTitulo">
+          <h3 className="card-title">{musicName}</h3>
+          <div className="corpoSTitulo">
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+            </audio>
+            <label htmlFor={ trackId }>
+              <input
+                type="checkbox"
+                id={ trackId }
+                name="Favorites"
+                data-testid={ `checkbox-music-${trackId}` }
+                onClick={ onInputClick }
+                defaultChecked={ isChecked }
+              />
+              Favorita
+            </label>
           </div>
         </div>
       </div>
